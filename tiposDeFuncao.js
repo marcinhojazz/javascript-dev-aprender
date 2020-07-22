61a954426fdc5c5b2bb09a4f55fdb4e5e36f9746
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-07-22 18:00:28 -0300

    AULA - TIPOS DE FUNÇÃO

---
+++ tiposDeFuncao.js
@@ -0,0 +1,16 @@
+// Realizar uma tarefa, não devolve nada
+function dizerNome(){
+    console.log('Marcio');
+}
+
+dizerNome();
+// Faz um cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor) {
+    return valor * 2;
+}
+
+// console.log(MultiplicarPorDois(5))
+
+let resultado = MultiplicarPorDois(5)
+
+console.log(resultado);
