ac417d80749336e947ed1fdd5fa7586c7069ee5f
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-07-22 17:54:56 -0300

    AULA - FUNCTIONS

---
+++ functions.js
@@ -0,0 +1,10 @@
+// Verbo + Substantivo
+
+let corSite = "azul";
+function resetaCor(cor, tonalidade){
+    corSite = cor + ' ' + tonalidade;
+};
+
+console.log(corSite);
+resetaCor("verde", "claro");
+console.log(corSite)
