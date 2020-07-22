8c4be1deea6d1cbee7f59709715d62b18401bfac
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-07-22 16:36:54 -0300

    Aula 04 - Tipagem Dinâmica

---
+++ tipagemDinamica.js
@@ -0,0 +1,11 @@
+// Tipagens Dinâmicas
+
+let nome = 'Marcio a11#@$##a'; // string literal
+let idade = 25; // number literal
+let estaAprovado = true; // boolean
+let sobrenome ; // Undefined
+let corSelecionada = null // Redefinir um valor quando necessário
+
+// usando typeof + nome da variável, no console ele diz que tipo é
+// typeof nome/idade/estaAprovado...
+    // "string" / "number" / "boolean" / ...
