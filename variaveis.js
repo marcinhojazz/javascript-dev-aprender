18ff9ce229674187bf78dc34793315564cb887ec
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-07-22 16:09:08 -0300

    AULA 2 - VARIÁVEIS

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+// VARIÁVEL let: Permite que você redefina quantas vezes for necessário.
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+
+
+
+// VARIÁVEI const: Não consegue redefinir uma variável que é constante. Usar para quando existem valores que não precisam ser redefinidos. É uma boa prática que se use preferencialmente o "const" caso não haja necessidade de alteração de um item.
+const valorIngressoAdulto2 = 20;
+console.log(valorIngressoAdulto2);
+
+// 
