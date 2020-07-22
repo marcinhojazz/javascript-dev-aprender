99095b9df8358c3f100334535a9a86be69d0e78e
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-07-22 16:29:46 -0300

    Aula 03 - Tipos Primitivos

---
+++ tiposPrimitivos.js
@@ -0,0 +1,8 @@
+// Tipos Primitivos
+
+let nome = 'Marcio a11#@$##a'; // string literal
+let idade = 25; // number literal
+let estaAprovado = true; // boolean
+let sobrenome ; // Undefined
+let corSelecionada = null // Redefinir um valor quando necessário
+
